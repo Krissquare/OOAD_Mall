92e9099e18ef6b5ccd588325e2ea9723e39b53db
mawangdan <mawangdan@foxmail.com>
2021-11-13 08:55:49 +0800

    1-19

--- core/src/test/java/cn/edu/xmu/oomall/core/util/CloneVoTest.java
+++ core/src/test/java/cn/edu/xmu/oomall/core/util/CloneVoTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 //@SpringBootTest(classes = CoreApplication.class)
 
-class CommonTest {
+class CloneVoTest {
 
     /**
      * @author xucangbai
