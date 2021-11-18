9a65f364c6868039255250f0da5b9933ceda24f1
Ming Qiu <mingqiu@xmu.edu.cn>
2021-11-18 01:44:46 +0000

    Revert "Merge branch 'main' into 'main'"
    This reverts merge request !159

--- coupon/src/main/java/cn/edu/xmu/oomall/coupon/model/bo/CouponActivity.java
+++ coupon/src/main/java/cn/edu/xmu/oomall/coupon/model/bo/CouponActivity.java
@@ -19,36 +19,36 @@ public class CouponActivity {
         /**
          * 草稿
          */
-        DRAFT((byte)0,"草稿"),
+        EXAME(0,"草稿"),
         /**
          * 上线
          */
-        ONLINE((byte)1,"上线"),
+        ONLINE(1,"上线"),
         /**
          * 下线
          */
-        OFFLINE((byte)2, "下线");
+        OFFLINE(2, "下线");
 
-        private static final Map<Byte, State> STATE_MAP;
+        private static final Map<Integer, State> STATE_MAP;
         static { //由类加载机制，静态块初始加载对应的枚举属性到map中，而不用每次取属性时，遍历一次所有枚举值
             STATE_MAP = new HashMap();
             for (State enum1 : values()) {
                 STATE_MAP.put(enum1.code, enum1);
             }
         }
-        private Byte code;
+        private int code;
         private String description;
 
-        State(Byte code, String description) {
+        State(int code, String description) {
             this.code = code;
             this.description = description;
         }
 
-        public static State getTypeByCode(Byte code) {
+        public static State getTypeByCode(Integer code) {
             return STATE_MAP.get(code);
         }
 
-        public Byte getCode() {
+        public Integer getCode() {
             return code;
         }
 
